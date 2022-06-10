1d87136f15539385a78c21df1dd9ee1121f7ada2
Reazor42 <97978472+Reazor42@users.noreply.github.com>
2022-06-10 10:29:51 +0200

    update

--- python/GUI_mit_Java/src/gui_package/Main.java
+++ python/GUI_mit_Java/src/gui_package/Main.java
@@ -1,10 +1,49 @@
 package gui_package;
 
-public class Main {
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+	
+	private Stage primaryStage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		mainWindow();
+	}
+	
+	public void mainWindow() {
+		try {
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
+			AnchorPane pane = loader.load();
+			
+			primaryStage.setMinWidth(1300);
+			primaryStage.setMinHeight(800);
+			
+			MainWindowController mwc = loader.getController();
+			mwc.setMain(this);
+			
+			Scene scene = new Scene(pane);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- python/GUI_mit_Java/src/gui_package/MainWindowController.java
+++ python/GUI_mit_Java/src/gui_package/MainWindowController.java
@@ -1,5 +1,11 @@
 package gui_package;
 
 public class MainWindowController {
+	
+	public Main main;
+	
+	public void setMain(Main main) {
+		this.main = main;
+	}
 
 }
