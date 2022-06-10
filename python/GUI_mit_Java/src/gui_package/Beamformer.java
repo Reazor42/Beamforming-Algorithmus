b4326d5652c9b3d9b4c3121227f052c0b804101c
Reazor42 <97978472+Reazor42@users.noreply.github.com>
2022-06-10 10:28:04 +0200

    quasi initial commit

--- python/GUI_mit_Java/src/gui_package/Beamformer.java
+++ python/GUI_mit_Java/src/gui_package/Beamformer.java
@@ -1,12 +1,5 @@
 package gui_package;
 
-
-//imports
-
-
-
 public class Beamformer {
-	
-	ProcessBuilder pb = new ProcessBuilder();
-	
+
 }
--- python/GUI_mit_Java/src/gui_package/Main.java
+++ python/GUI_mit_Java/src/gui_package/Main.java
@@ -1,13 +1,10 @@
 package gui_package;
 
-
-
-
-
 public class Main {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }
