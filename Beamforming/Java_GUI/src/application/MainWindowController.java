d6679956c7bb1de90918d1fe5a54ea6babdfc689
Reazor42 <97978472+Reazor42@users.noreply.github.com>
2022-06-26 18:11:19 +0200

    Grundlagen update (noch nicht die 'richtige' GUI)

--- Beamforming/Java_GUI/src/application/MainWindowController.java
+++ Beamforming/Java_GUI/src/application/MainWindowController.java
@@ -1,13 +1,16 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainWindowController {
 	
 	/**
 	 * Views
 	 */
-	
+	@FXML private Label pathLabel;
+	@FXML private TextField pathField;
 	
 	public Main main;
 	
@@ -18,12 +21,14 @@ public class MainWindowController {
 	
 	@FXML
 	public void submit() {
-		
+		String text = pathField.getText();
+		pathLabel.setText(text);
 	}
 	
 	@FXML
 	public void cancel() {
-		
+		pathLabel.setText("Text eingeben:");
+		pathField.clear();
 	}
 
 }
